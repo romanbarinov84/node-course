8a942b0836e5cfcf393e1414e625b4b3cf27f5f1
Roman <roman-meinl@ukr.net>
2026-09-14 18:32:51 +0200

    step one "simple function"

---
+++ lesson_02/app.js
@@ -0,0 +1,7 @@
+
+const {add ,multiply} = require(`./calculator`)
+
+console.log(add(8 , 4));
+
+console.log(multiply(5 , 3));
+
---
+++ lesson_02/calculator.js
@@ -0,0 +1,10 @@
+
+const add = function(a ,b){
+    return a + b 
+}
+
+const multiply = function(a , b){
+    return a * b
+}
+
+module.exports = {add,multiply}
