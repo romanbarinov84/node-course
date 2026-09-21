ac43dbc1f01fbb6ba4ea3d49ebc107f655b28206
Roman <roman-meinl@ukr.net>
2026-09-21 19:02:49 +0200

    read and write file 21.09.26

--- block-3/index.js
+++ block-3/index.js
@@ -1,21 +1,32 @@
+
 const fs = require('fs');
+const path = require('path');
 
 
+const pathToTextFile = path.join('test','data','text.txt');
 
-console.log('start');
 
-
- fs.readFile('./test/data/text.txt','utf-8',(err,data)=>{
+fs.writeFile(pathToTextFile , 'I must be of , because my britches is very dirty',(err) => {
     if(err){
-        console.log(err);
-        
+        console.log('write operation was failed',err);
+        return
     }
-    console.log('file:', data);
+   else {
+        console.log(' File was written');
+        fs.readFile(pathToTextFile,'utf-8',(err,data) => {
+            if(err){
+                console.log('reading operation is failed');
+                return;
+            }
+            {
+                console.log('File was read');
+               
+            }
+            console.log(data);
+            
+            
+        })
+    }
     
-});
-
-
-
-
-console.log('end');
-
+    
+})
