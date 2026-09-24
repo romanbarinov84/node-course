d4a6213d1c89d42a5d8e35c8a74f2de8616ba811
Roman <roman-meinl@ukr.net>
2026-09-24 17:56:05 +0200

    os.cpus() + foreach

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 
-const fs = require('fs');
-const path = require('path');
+const os = require('os');
 
+const cpus = os.cpus();
 
-function workFile(){
-    const pathToFile = path.join(__dirname,'test','data','text.txt');
+cpus.forEach((item,index) => {
 
-    fs.writeFile(pathToFile,'I love Node.js',(err) => {
-        if(err){
-            console.log(`Cannot write this file : ${pathToFile}`);
-            return
-        }
-     fs.readFile(pathToFile,'utf-8',(err,data) => {
-        if(err){
-            console.log(`Cannot read this file : ${pathToFile}`);
-            return
-        }
-        console.log('File was reading');
-        console.log(data);
-        
-     })
-    })
-}
-
-workFile()
+    console.log(`CPU${index}: ${item.model} "|" Speed:  ${item.speed}`);
+    
+})
